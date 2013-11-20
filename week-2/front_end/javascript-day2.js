eb5166f43057b1f4860e1d28dafc6f8a369859e5
Jorge Polanco <dr.jorgepolanco@gmail.com>
2013-11-20 14:45:36 -0600

    Javascript files

---
+++ week-2/front_end/javascript-day2.js
@@ -0,0 +1,47 @@
+<html>
+<head>
+<body>
+
+<script type="text/javascript">
+
+var hello = function (x) {
+	return 8;
+};
+
+var result = hello(5)
+	console.log("Result is: ", result);
+	
+
+var multiply = function (x, y) {
+	return x * y;
+};
+
+var result = multiply(5, 4)
+	console.log("Result is: ", result);
+
+var m = multiply;
+
+var other result
+
+
+var multiply = function(x, y) {
+	return x * y;
+};
+
+var func = multiply(5)
+var func = NaN
+
+undefined = nothing
+null = your own nil
+
+NaN = Not a Number
+
+
+</script>
+
+
+</body>
+
+
+</head>
+</html>
